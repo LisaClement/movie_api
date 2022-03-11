3130587e61d691e55a2b5308e43ffa19bff096a7
Lisa Clement <lisa.clement.a@gmail.com>
2022-03-11 19:33:03 +0100

    Remove movies & users variable

--- index.js
+++ index.js
@@ -23,73 +23,7 @@ app.use(morgan('common'));
 //make all files in /public available
 app.use(express.static('public'));
  
-<<<<<<< HEAD
 
-// GET request for home page
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie club!');
-});
-=======
-
-
-let users = [
-  {
-    id: 1,
-    name: 'Pierre', 
-    email: 'pierre@gmail.com'
-   },
-]
-
-let movies = [
-  {
-   title: 'The Notebook', 
-   director: 'Nick Cassavetes',
-   genre: 'Romance',
-   description: 'A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.',
-   year: 2004,
-   imdb_rating: 7.8
-  },
-  {
-   title: 'Deadpool',
-   director: 'Tim Miller',
-   genre: 'Super heroes',
-   description: 'A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.',
-   year: 2016,
-   imdb_rating: 8.0
-  },
-  {
-   title: 'Free Guy',
-   director: 'Shawn Levy',
-   genre:'Comedy',
-   description: 'A bank teller discovers that he is actually an NPC inside a brutal, open world video game.',
-   year: 2021,
-   imdb_rating: 7.2
-  },
-  {
-   title: 'A Star Is Born', 
-   director: 'Bradley Cooper',
-   genre:'Romance',
-   description:'A musician helps a young singer find fame as age and alcoholism send his own career into a downward spiral.',
-   year: 2018,
-   imdb_rating: 7.6
-  },
->>>>>>> c667f1f987d372795f258fb326db893ba6b71f52
-
-
-<<<<<<< HEAD
-// Get all users
-app.get('/users', (req,res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error:' + err);
-    });
-});
-
-=======
 // GET request for home page
 app.get('/', (req, res) => {
   res.send('Welcome to my movie club!');
@@ -108,7 +42,6 @@ app.get('/users', (req,res) => {
     });
 });
 
->>>>>>> c667f1f987d372795f258fb326db893ba6b71f52
 
 // Post request to create a new user
 app.post('/users/register', (req,res) => {
@@ -161,7 +94,6 @@ app.get('/users/:Username', (req, res) => {
   (required)
   Birthday: Date
 }*/
-<<<<<<< HEAD
 
 app.put('/users/:Username', (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
@@ -183,29 +115,6 @@ app.put('/users/:Username', (req, res) => {
   });
 });
 
-=======
-
-app.put('/users/:Username', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-    {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
-    }
-  },
-  { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if(err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
-    }
-  });
-});
-
->>>>>>> c667f1f987d372795f258fb326db893ba6b71f52
 // endpoint to allow users to deregister
 app.delete('/users/deregister/:Username', (req, res) => {
   Users.findOneAndRemove({ Username: req.params.Username})
@@ -269,7 +178,6 @@ app.get('/directors/:Director', (req, res) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
-<<<<<<< HEAD
 });
 
 // endpoint to allow users to add a movie to their favorites
@@ -289,27 +197,6 @@ app.post('/users/:Username/movies/:MovieID', (req, res) => {
     });
 });
 
-=======
-});
-
-// endpoint to allow users to add a movie to their favorites
-app.post('/users/:Username/movies/:MovieID', (req, res) => {
-  Users.findOneAndUpdate ( { Username: req.params.Username}, {
-    $push: { FavoriteMovies: req.params.MovieID }
-  },
-  { new: true}, // This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-        
-      } else {
-        res.json(updatedUser);
-      }
-    });
-});
-
->>>>>>> c667f1f987d372795f258fb326db893ba6b71f52
 // endpoint to allow users to remove a movie from their favorites
 
 app.delete('/users/:Username/movies/:MovieID', (req, res) => {
@@ -341,3 +228,5 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
+© 2022 GitHub, Inc.
+Terms
